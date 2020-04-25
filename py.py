425293b102018dcbe35909668e64b9253948b38b
Chao Yang <chao.yang@coretex.com>
2020-04-26 06:44:57 +1200

    test access token

--- py.py
+++ py.py
@@ -15,6 +15,7 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
+
 # Load dataset
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
